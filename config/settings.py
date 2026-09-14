1b9a9782aac20b4afc2d0048ff720d09ce37f111
Rakshita Patil <27rakshitapatil@gmail.com>
2026-09-14 16:59:36 +0530

    Configure secure email settings

--- config/settings.py
+++ config/settings.py
@@ -139,3 +139,15 @@ EMAIL_HOST_PASSWORD = 'wsqv fcuq bjur ujfj'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+# Email Configuration
+import os
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = os.environ.get("EMAIL_HOST", "smtp.gmail.com")
+EMAIL_PORT = int(os.environ.get("EMAIL_PORT", 587))
+EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS", "True") == "True"
+
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
