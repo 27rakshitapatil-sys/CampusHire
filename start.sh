e6f4dfd6f92b288c71d1784c46016fe91bf57ebb
Rakshita Patil <27rakshitapatil@gmail.com>
2026-09-15 18:32:33 +0530

    Fix production login account

--- start.sh
+++ start.sh
@@ -1,3 +1,24 @@
 #!/usr/bin/env bash
+
+python manage.py shell -c "
+import os
+from django.contrib.auth.models import User
+
+username = os.environ.get('CAMPUSHIRE_LOGIN_USERNAME')
+password = os.environ.get('CAMPUSHIRE_LOGIN_PASSWORD')
+
+if username and password:
+    user, created = User.objects.get_or_create(
+        username=username,
+        defaults={'email': 'rakshitapatil@gmail.com'}
+    )
+
+    user.set_password(password)
+    user.save()
+
+    print('CampusHire login account ready.')
+"
+
 python manage.py seed_jobs
+
 gunicorn config.wsgi:application
